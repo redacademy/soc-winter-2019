9cfcf62a6c6c922163764a64557b0a93767d9c0a
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-12 12:07:44 -0700

    Added events custom post type

--- plugins/sports-on-campus-functionality/lib/functions/post-types.php
+++ plugins/sports-on-campus-functionality/lib/functions/post-types.php
@@ -63,3 +63,59 @@ function university() {
 
 }
 add_action( 'init', 'university', 0 );
+
+// Register Event Post Type
+function events() {
+
+	$labels = array(
+		'name'                  => 'Events',
+		'singular_name'         => 'Event',
+		'menu_name'             => 'Events',
+		'name_admin_bar'        => 'Events',
+		'archives'              => 'Event Archives',
+		'attributes'            => 'Event Attributes',
+		'parent_item_colon'     => 'Parent Event:',
+		'all_items'             => 'All Events',
+		'add_new_item'          => 'Add NewEvent',
+		'add_new'               => 'Add New Event',
+		'new_item'              => 'New Event',
+		'edit_item'             => 'Edit Event',
+		'update_item'           => 'Update Event',
+		'view_item'             => 'View Event',
+		'view_items'            => 'View Events',
+		'search_items'          => 'Search Event',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Event',
+		'uploaded_to_this_item' => 'Uploaded to this event',
+		'items_list'            => 'Events list',
+		'items_list_navigation' => 'Events list navigation',
+		'filter_items_list'     => 'Filter Events list',
+	);
+	$args = array(
+		'label'                 => 'Event',
+		'description'           => 'Post Type for Events',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'event', $args );
+
+}
+add_action( 'init', 'events', 0 );
