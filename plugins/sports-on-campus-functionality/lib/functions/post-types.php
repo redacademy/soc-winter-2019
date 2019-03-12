52109c5a7bbf03578f4db418fb68c622325efc6c
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-12 13:21:49 -0700

    Fixed event function error in custom post type

--- plugins/sports-on-campus-functionality/lib/functions/post-types.php
+++ plugins/sports-on-campus-functionality/lib/functions/post-types.php
@@ -102,9 +102,22 @@ function events() {
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor' ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
 
 	);
-	register_post_type( 'events', $args );
+	register_post_type( 'event', $args );
 
 }
 add_action( 'init', 'events', 0 );
