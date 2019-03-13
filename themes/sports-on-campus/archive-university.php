da942dc7f51c2c64c57da11939f22a20afbb24ee
Naiji Zhang <naijizhang@gmail.com>
2019-03-13 11:00:40 -0700

    Add city information on archive university page

--- themes/sports-on-campus/archive-university.php
+++ themes/sports-on-campus/archive-university.php
@@ -10,20 +10,22 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <?php $universities =  soc_get_universities(); ?>
-        <?php foreach ($universities as $university) : setup_postdata($university); ?>
+        <?php foreach ($universities as $post) : setup_postdata($post); ?>
         <article id="university-<?php the_ID(); ?>" <?php post_class(); ?>>
             <?php if (has_post_thumbnail()) : ?>
-            <?php the_post_thumbnail('large'); ?>
+            <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
+                <?php the_post_thumbnail('large'); ?>
+            </a>
             <?php endif; ?>
-            <div class="university-title-button">
-                <h3 class="entry-title">
-                    <a href="<?php echo esc_url(get_the_permalink($university)) ?>" rel="bookmark">
-                    <?php echo get_the_title($university)?>
-                    </a>
-                </h3>
-                <input type="button" value="READ MORE" class="homebutton" id="go-to-post" onClick="document.location.href='<?php echo get_the_permalink($university) ?>'" />
-            </div><!-- .entry-button -->
-        </article><!-- #post-## -->
+            <div class="university-title-location">
+                <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
+                    <h3 class="university-name">
+                        <?php echo the_title() ?>
+                    </h3>
+                    <p class="university-city"><?php echo CFS()->get('city'); ?></p>
+                </a>
+            </div>
+        </article>
         <?php endforeach;
     wp_reset_postdata(); ?>
 
