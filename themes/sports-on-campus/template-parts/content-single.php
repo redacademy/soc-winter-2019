2f134dfebd63d4468dc4f369ee4b9e7c7e30bf44
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-13 10:03:59 -0700

    Added single-university.php template

--- themes/sports-on-campus/template-parts/content-single.php
+++ themes/sports-on-campus/template-parts/content-single.php
@@ -15,19 +15,10 @@
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
