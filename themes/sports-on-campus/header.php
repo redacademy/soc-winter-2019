fbf341e608a18f820c11c9961da0edb585bbef9d
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-14 16:25:41 -0700

    Mobile Navigation hamburger and styling

--- themes/sports-on-campus/header.php
+++ themes/sports-on-campus/header.php
@@ -29,9 +29,10 @@
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">&#9776;</i></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+	
