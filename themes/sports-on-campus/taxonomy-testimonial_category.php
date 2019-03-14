0c62d826a82fa7dca8fa6ce629195e05795a81d7
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-14 10:15:47 -0700

    Added taxonomy-testimonial_category and filtered respective testimonials

---
+++ themes/sports-on-campus/taxonomy-testimonial_category.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *Template name: Taxonomy Testimonial
+ * @package Sport_On_Campus_Theme
+ */
+
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+           
+
+	<?php if ( have_posts() ) : ?>
+
+<header class="page-header">
+	<?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="taxonomy-description">', '</div>' );
+	?>
+</header><!-- .page-header -->
+
+<?php /* Start the Loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+
+		<article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
+		            <?php if (has_post_thumbnail()) : ?>
+		            <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
+		                 <?php the_post_thumbnail('large'); ?>
+	                </a>
+		            <?php endif; ?>
+                         <h1>
+                             <?php the_title();?>
+                        </h1>
+                        <?php the_content();?>
+                        <div class="test-video"> <?php echo CFS()->get('video'); ?> </div>
+                        <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
+                        <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
+                </article>
+
+	
+<?php endwhile; ?>
+<?php endif; ?>
+
+          
+        </main><!-- #main -->
+    </div>
+
+
+<?php get_footer(); ?>
