0612232c1b2dfc8b5aae94dc5b2dd6ca0ce06a53
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-03-15 15:53:26 -0700

    Resolving merging conflicts

--- themes/sports-on-campus/archive-testimonial.php
+++ themes/sports-on-campus/archive-testimonial.php
@@ -6,80 +6,39 @@
  */
 
 get_header(); ?>
-<<<<<<< HEAD
-
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-            <?php if ( have_posts() ) : ?>
-            <section>
-                    <?php $testimonials=get_terms("testimonial_category");?>
-                    <?php foreach ($testimonials as $value): setup_postdata($value);?>
-                        <a href=<?php echo get_term_link($value)?>> <?php echo $value->name?></a>
-                     <?php endforeach; wp_reset_postdata();?>
-                     <?php $testimonials=soc_get_testimonial_category();?>
-                    <?php foreach ($testimonials as $post) : setup_postdata($post); ?>
-                    <article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <?php if (has_post_thumbnail()) : ?>
-                        <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
-                             <?php the_post_thumbnail('large'); ?>
-                        </a>
-                        <?php endif; ?>
-                         <h1>
-                             <?php the_title();?>
-                        </h1>
-                        <?php the_content();?>
-<!--                         <div class="test-video"> <?php echo CFS()->get('video'); ?> </div> -->
-                        <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
-                        <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
-                    </article>
-                    <?php endforeach; wp_reset_postdata();?>
-            </section>
-            <?php else : ?>
-            <?php get_template_part( 'template-parts/content', 'none' ); ?>
-            <?php endif; ?>
-        </main><!-- #main -->
-    </div>
-
-
-<?php get_footer(); ?>
-=======
 <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-        <?php if (have_posts()) : ?>
-        <section>
-            <?php $testimonials = get_terms("testimonial_category"); ?>
-            <?php foreach ($testimonials as $value) : setup_postdata($value); ?>
-            <a href=<?php echo get_term_link($value) ?>><?php echo $value->name ?></a>
-            <?php endforeach;
-        wp_reset_postdata(); ?>
-            <?php $testimonials = soc_get_testimonial_category(); ?>
-            <?php foreach ($testimonials as $post) : setup_postdata($post); ?>
-            <article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <?php if (has_post_thumbnail()) : ?>
-                <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
-                    <?php the_post_thumbnail('large'); ?>
-                </a>
-                <?php endif; ?>
-                <h1>
-                    <?php the_title(); ?>
-                </h1>
-                <?php the_content(); ?>
-                <!--                         <div class="test-video"> <?php  ?> </div> -->
-                                        <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
-                                        <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
-                             </article>
-                         <?php endforeach;
-                        wp_reset_postdata(); ?>
-                
-                            
-
-                        </section>
-        <?php else : ?>
-        <?php get_template_part('template-parts/content', 'none'); ?>
-        <?php endif; ?>
-    </main><!-- #main -->
+   <main id="main" class="site-main" role="main">
+       <?php if (have_posts()) : ?>
+       <section >
+           <?php $testimonials = get_terms("testimonial_category"); ?>
+           <?php foreach ($testimonials as $value) : setup_postdata($value); ?>
+           <a href=<?php echo get_term_link($value) ?>><?php echo $value->name ?></a>
+           <?php endforeach;
+           wp_reset_postdata(); ?>
+           <?php $testimonials = soc_get_testimonial_category(); ?>
+           <?php foreach ($testimonials as $post) : setup_postdata($post); ?>
+           <article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
+               <?php if (has_post_thumbnail()) : ?>
+               <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
+                   <?php the_post_thumbnail('large'); ?>
+               </a>
+               <?php endif; ?>
+               <img class="test-image sample-picture" src='<?php echo CFS()->get('video'); ?>'>
+               <h1>
+                   <?php the_title(); ?>
+               </h1>
+               <?php the_content(); ?>
+                               <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
+                               <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
+           </article>
+                        <?php endforeach;
+                       wp_reset_postdata(); ?>
+       </section>
+       <?php else : ?>
+       <?php get_template_part('template-parts/content', 'none'); ?>
+       <?php endif; ?>
+   </main><!-- #main -->
 </div>
 
 
-<?php get_footer(); ?> 
->>>>>>> 3d8270590a903cea4b8957d38361abf8e0e3cd79
+<?php get_footer(); ?>
