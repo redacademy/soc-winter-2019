e3957006e56c1749bc6f45f4ab8e0afe6493e1cb
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-15 13:38:09 -0700

    Fixing merge conflict

--- themes/sports-on-campus/footer.php
+++ themes/sports-on-campus/footer.php
@@ -11,13 +11,9 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
-				<img src=<?php echo get_template_directory_uri() . '/icons/twitter-footer.svg'?>>
-				<img src=<?php echo get_template_directory_uri() . '/icons/facebook-footer.svg'?>>
-				<img src=<?php echo get_template_directory_uri() . '/icons/instagram-footer.svg'?>>
+					<img src=<?php echo get_template_directory_uri() . '/icons/twitter-footer.svg'?>>
+					<img src=<?php echo get_template_directory_uri() . '/icons/facebook-footer.svg'?>>
+					<img src=<?php echo get_template_directory_uri() . '/icons/instagram-footer.svg'?>>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
