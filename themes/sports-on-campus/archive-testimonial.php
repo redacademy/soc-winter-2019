3f6c30899bbdc0809cd548c0bfe0574f5632c08b
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-03-15 15:40:25 -0700

    Starting front blog page styling

--- themes/sports-on-campus/archive-testimonial.php
+++ themes/sports-on-campus/archive-testimonial.php
@@ -10,17 +10,12 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <?php if ( have_posts() ) : ?>
-
             <section>
-                
-        
                     <?php $testimonials=get_terms("testimonial_category");?>
                     <?php foreach ($testimonials as $value): setup_postdata($value);?>
                         <a href=<?php echo get_term_link($value)?>> <?php echo $value->name?></a>
                      <?php endforeach; wp_reset_postdata();?>
-
                      <?php $testimonials=soc_get_testimonial_category();?>
-
                     <?php foreach ($testimonials as $post) : setup_postdata($post); ?>
                     <article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
                         <?php if (has_post_thumbnail()) : ?>
@@ -37,18 +32,10 @@ get_header(); ?>
                         <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
                     </article>
                     <?php endforeach; wp_reset_postdata();?>
-    
-                
-
             </section>
             <?php else : ?>
             <?php get_template_part( 'template-parts/content', 'none' ); ?>
             <?php endif; ?>
-
-    
-
-
-            
         </main><!-- #main -->
     </div>
 
