73b780a066b228817894026d8aec189d4fd09e21
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-16 18:50:20 -0700

    Styling on testimonial categories

--- themes/sports-on-campus/taxonomy-testimonial_category.php
+++ themes/sports-on-campus/taxonomy-testimonial_category.php
@@ -5,35 +5,36 @@
  * @package Sport_On_Campus_Theme
  */
 
-
 get_header(); ?>
-
-   <div id="primary" class="content-area">
+<div id="primary" class="content-area">
        <main id="main" class="site-main" role="main">
           <?php if ( have_posts() ) : ?>
-			<?php $testimonials=get_terms("testimonial_category");?>		
-            <?php foreach ($testimonials as $value): setup_postdata($value);?>
-            <a href=<?php echo get_term_link($value)?>> <?php echo $value->name?></a>
-            <?php endforeach; wp_reset_postdata();?>
+			<div class="mobile-slide">
+                <?php $testimonials = get_terms("testimonial_category"); ?>
+                <?php foreach ($testimonials as $value) : setup_postdata($value); ?>
+                <a href=<?php echo get_term_link($value) ?>><?php echo $value->name ?></a>
+                <?php endforeach;wp_reset_postdata(); ?>
+                <?php $testimonials = soc_get_testimonial_category(); ?>
+            </div>
 			<?php while ( have_posts() ) : the_post(); ?>
-			<article id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
-		            <?php if (has_post_thumbnail()) : ?>
-		            <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
-	                <?php the_post_thumbnail('large'); ?>
-	                </a>
-		            <?php endif; ?>
-                    <h1>
-                       <?php the_title();?>
-                    </h1>
-                     <?php the_content();?>
-                        <div class="test-video"> <?php echo CFS()->get('video'); ?> </div>
-                        <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
-                        <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
-                </article>	
+			<article  id="testimonial-<?php the_ID(); ?>" <?php post_class(); ?>>
+               <?php if (has_post_thumbnail()) : ?>
+               <a href="<?php echo esc_url(the_permalink()) ?>" rel="bookmark">
+                   <?php the_post_thumbnail('large'); ?>
+               </a>
+               <?php endif; ?>
+               <img class="test-image sample-picture" src='<?php echo CFS()->get('video'); ?>'>
+               <h1>
+                   <?php the_title(); ?>
+               </h1>
+                               <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
+                               <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
+                               <?php the_content(); ?>
+           </article>
 				<?php endwhile; ?>
 				<?php endif; ?>  
         </main><!-- #main -->
-   </div>
+</div>
 
 
 <?php get_footer(); ?>
