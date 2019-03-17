8aa29f2b5b72c531024bb79c66c9a90e5282a6eb
Naiji Zhang <naijizhang@gmail.com>
2019-03-16 19:22:43 -0700

    Add front page sport select and icons

--- themes/sports-on-campus/front-page.php
+++ themes/sports-on-campus/front-page.php
@@ -11,25 +11,39 @@ get_header(); ?>
 
         <?php while (have_posts()) : the_post(); ?>
 
-        <?php the_content();?>
+        <?php the_content(); ?>
 
         <?php endwhile; ?>
 
+        <section class="sport-select-container">
+            <h3>Start with what you know</h3>
+            <h2>Choose Sport</h2>
+            <?php $sports = get_terms('university_sport', array(
+                'hide_empty' => false,
+                'orderby' => 'name',
+                'order' => 'ASC'
+            )); ?>
+            <ul class="sport-list">
+                <?php foreach ($sports as $sport) : ?>
+                <li class="each-item <?php echo $sport->name ?>-menu">
+                    <img src=<?php echo get_template_directory_uri() . "/assets/images/" . $sport->name . ".png" ?>>
+                    <p>
+                        <?php echo $sport->name ?>
+                    </p>
+                </li>
+                <?php endforeach ?>
+            </ul>
+            <button>Search</button>
+        </section>
 
-        <?php $testimonials = get_terms("testimonial_category"); ?>
-        <?php foreach ($testimonials as $value) : setup_postdata($value); ?>
-        <a href=<?php echo get_term_link($value) ?>> <?php echo $value->name ?></a>
-        <?php endforeach;
-    wp_reset_postdata(); ?>
-
-
+        <h1>Testimonials</h1>
         <section class="front-page-testimonials">
             <div class="main-carousel testimonial-container" data-flickity='{ "autoPlay": true }'>
 
 
 
                 <?php $testimonials = soc_get_testimonial_category(); ?>
-                <?php foreach ($testimonials as $post) : setup_postdata($post); ?> 
+                <?php foreach ($testimonials as $post) : setup_postdata($post); ?>
 
                 <div class="carousel-cell testimonial-item" <?php post_class(); ?>>
                     <?php if (has_post_thumbnail()) : ?>
@@ -38,11 +52,12 @@ get_header(); ?>
                     </a>
                     <?php endif; ?>
                     <img class="test-image sample-picture" src='<?php echo CFS()->get('video'); ?>'>
-                    <h1>
+                    <h2>
                         <?php the_title(); ?>
-                    </h1>
+                    </h2>
                     <div class="test-position"> <?php echo CFS()->get('position'); ?> </div>
                     <div class="test-uni"> <?php echo CFS()->get('university'); ?> </div>
+                    <i class="fas fa-quote-left"></i>
                     <?php the_content(); ?>
                 </div>
 
