7c68a91dbdaa0fd8c00eea4251d7f2a6ee790288
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-03-18 10:07:16 -0700

    Continuing on front blog page styling

--- themes/sports-on-campus/template-parts/content.php
+++ themes/sports-on-campus/template-parts/content.php
@@ -9,21 +9,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+		<div class="blog-container">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'large' ); ?>
+			<?php endif; ?>
 
-		<div class="blog-text">
-			<div class="entry-meta">
-				<?php Sport_On_Campus_posted_on(); ?>
-			</div><!-- .entry-meta -->
+			<div class="blog-text">
+				<div class="entry-meta">
+					<?php Sport_On_Campus_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			</div>
+				<?php if ( 'post' === get_post_type() ) : ?>
 
-			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				<?php endif; ?>
 		</div>
-		<?php if ( 'post' === get_post_type() ) : ?>
-
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 </article><!-- #post-## -->
