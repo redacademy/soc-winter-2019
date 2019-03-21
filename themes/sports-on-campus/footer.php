e37ad156844b296e0f82639694156a258f98228a
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-21 10:02:22 -0700

    Desktop footer styling

--- themes/sports-on-campus/footer.php
+++ themes/sports-on-campus/footer.php
@@ -10,8 +10,8 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php wp_nav_menu( array( 'theme_location' => 'desktop-menu-two', 'container_class' => 'desktop-menu-two' ) ); ?>
-			<h4>Copyright 2019</h4>
+			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer-menu' ) ); ?>
+			<h5>Copyright 2019</h5>
 					<div class="nav-dropdown">
 				<div class="site-info">
 					<img src=<?php echo get_template_directory_uri() . '/icons/twitter-footer.svg'?>>
