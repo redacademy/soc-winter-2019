3885ae7c47c5cebba4134c98484ef7a73951f235
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-25 12:01:47 -0700

    Header our story

--- themes/sports-on-campus/template-parts/content-single.php
+++ themes/sports-on-campus/template-parts/content-single.php
@@ -9,10 +9,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
 	<header class="entry-header">
+
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
+		<div class="entry-meta">
+					<h2>Published on <?php Sport_On_Campus_posted_on(); ?></h2>
+		</div><!-- .entry-meta -->
+	
 
 	</header><!-- .entry-header -->
 
