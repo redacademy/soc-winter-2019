74b5b276a92ad198484ddb144b6ee934e17e55ad
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-03-26 14:14:49 -0700

    lowercase class names

--- themes/sports-on-campus/js/menu.js
+++ themes/sports-on-campus/js/menu.js
@@ -14,44 +14,44 @@
 
     //front page sport select
     let select = '';
-    $('.Basketball-menu').on('click', function() {
-      $('.Basketball-menu p').addClass('orange-underscore');
-      $('.Rugby-menu p').removeClass('orange-underscore');
-      $('.Other-menu p').removeClass('orange-underscore');
-      $('.Soccer-menu p').removeClass('orange-underscore');
-      $('.Volleyball-menu p').removeClass('orange-underscore');
+    $('.basketball-menu').on('click', function() {
+      $('.basketball-menu p').addClass('orange-underscore');
+      $('.rugby-menu p').removeClass('orange-underscore');
+      $('.other-menu p').removeClass('orange-underscore');
+      $('.soccer-menu p').removeClass('orange-underscore');
+      $('.volleyball-menu p').removeClass('orange-underscore');
       select = 'Basketball';
     });
-    $('.Rugby-menu').on('click', function() {
-      $('.Basketball-menu p').removeClass('orange-underscore');
-      $('.Rugby-menu p').addClass('orange-underscore');
-      $('.Other-menu p').removeClass('orange-underscore');
-      $('.Soccer-menu p').removeClass('orange-underscore');
-      $('.Volleyball-menu p').removeClass('orange-underscore');
+    $('.rugby-menu').on('click', function() {
+      $('.basketball-menu p').removeClass('orange-underscore');
+      $('.rugby-menu p').addClass('orange-underscore');
+      $('.other-menu p').removeClass('orange-underscore');
+      $('.soccer-menu p').removeClass('orange-underscore');
+      $('.volleyball-menu p').removeClass('orange-underscore');
       select = 'Rugby';
     });
-    $('.Other-menu').on('click', function() {
-      $('.Basketball-menu p').removeClass('orange-underscore');
-      $('.Rugby-menu p').removeClass('orange-underscore');
-      $('.Other-menu p').addClass('orange-underscore');
-      $('.Soccer-menu p').removeClass('orange-underscore');
-      $('.Volleyball-menu p').removeClass('orange-underscore');
+    $('.other-menu').on('click', function() {
+      $('.basketball-menu p').removeClass('orange-underscore');
+      $('.rugby-menu p').removeClass('orange-underscore');
+      $('.other-menu p').addClass('orange-underscore');
+      $('.soccer-menu p').removeClass('orange-underscore');
+      $('.volleyball-menu p').removeClass('orange-underscore');
       select = 'Other';
     });
-    $('.Soccer-menu').on('click', function() {
-      $('.Basketball-menu p').removeClass('orange-underscore');
-      $('.Rugby-menu p').removeClass('orange-underscore');
-      $('.Other-menu p').removeClass('orange-underscore');
-      $('.Soccer-menu p').addClass('orange-underscore');
-      $('.Volleyball-menu p').removeClass('orange-underscore');
+    $('.soccer-menu').on('click', function() {
+      $('.basketball-menu p').removeClass('orange-underscore');
+      $('.rugby-menu p').removeClass('orange-underscore');
+      $('.other-menu p').removeClass('orange-underscore');
+      $('.soccer-menu p').addClass('orange-underscore');
+      $('.volleyball-menu p').removeClass('orange-underscore');
       select = 'Soccer';
     });
-    $('.Volleyball-menu').on('click', function() {
-      $('.Basketball-menu p').removeClass('orange-underscore');
-      $('.Rugby-menu p').removeClass('orange-underscore');
-      $('.Other-menu p').removeClass('orange-underscore');
-      $('.Soccer-menu p').removeClass('orange-underscore');
-      $('.Volleyball-menu p').addClass('orange-underscore');
+    $('.volleyball-menu').on('click', function() {
+      $('.basketball-menu p').removeClass('orange-underscore');
+      $('.rugby-menu p').removeClass('orange-underscore');
+      $('.other-menu p').removeClass('orange-underscore');
+      $('.soccer-menu p').removeClass('orange-underscore');
+      $('.volleyball-menu p').addClass('orange-underscore');
       select = 'Volleyball';
     });
     $('.front-page-search').on('click', function() {
--- themes/sports-on-campus/template-parts/content.php
+++ themes/sports-on-campus/template-parts/content.php
@@ -10,7 +10,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<div class="blog-container">
+		
 			<?php if ( has_post_thumbnail() ) : ?>
+			
 				<?php the_post_thumbnail( 'large' ); ?>
 			<?php endif; ?>
 
