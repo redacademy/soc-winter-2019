3a1536d9e15dd2d281ae4502ffd6f076f646fbba
Naiji Zhang <naijizhang@gmail.com>
2019-03-26 14:34:45 -0700

    fix html calidation on single university and archive university

--- themes/sports-on-campus/single-university.php
+++ themes/sports-on-campus/single-university.php
@@ -8,52 +8,55 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+    <main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-		
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="entry-header">
-				<?php if ( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( 'large' ); ?>
-				<?php endif; ?>
-				<div class="ubc-intro">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
-			<div class="entry-content">
-				<div class="uni-block">
-				<div class="wp-block-button"><a class="wp-block-button__link" href="https://soc.van.cp.academy.red/join-us/">Connect with a Coach</a></div>
-				<ul class="sport-list"> 
-					<li class="soccer"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Soccer.png" alt="soccer-ball"> </li>
-					<li class="football"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Rugby.png" alt="football"></li>
-					<li class="basketball"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Basketball.png" alt="basketball"></li>
-					<li class="volleyball"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Volleyball.png" alt="volleyball "></li>
-				</ul>
-</div>
-				<?php the_content(); ?>
-			</div><!-- .entry-content -->
-			</section>			
-			<h3>programs & more</h3>
-			<div class="uni-program"> <?php echo CFS()->get('program'); ?> 
-				<div class="read-more"><p>Read more</p></div>
-			</div>
-			<div class="uni-prerequisites">
-				<h3 class="secondary-heading">prerequisites for application</h3>
-				<div><?php echo CFS()->get('prerequisites'); ?></div>
-			</div>
-				<h3>facilities and quick links</h3>
-				<div class="uni-facilities"> <?php echo CFS()->get('facilities'); ?> </div>
-				<div class="uni-maps"> <?php echo CFS()->get('maps'); ?> </div>
 
-				<footer class="entry-footer">
-					<?php Sport_On_Campus_entry_footer(); ?>
-				</footer><!-- .entry-footer -->
-		</article><!-- #post-## -->
-			<?php endwhile; // End of the loop. ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php if (has_post_thumbnail()) : ?>
+                <?php the_post_thumbnail('large'); ?>
+                <?php endif; ?>
+                <div class="ubc-intro">
+                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                </div>
+            </header><!-- .entry-header -->
+            <div class="entry-content">
+                <div class="uni-block">
+                    <div class="wp-block-button"><a class="wp-block-button__link" href="https://soc.van.cp.academy.red/join-us/">Connect with a Coach</a></div>
+                    <ul class="sport-list">
+                        <li class="soccer"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Soccer.png" alt="soccer-ball"> </li>
+                        <li class="football"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Rugby.png" alt="football"></li>
+                        <li class="basketball"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Basketball.png" alt="basketball"></li>
+                        <li class="volleyball"><img src="https://soc.van.cp.academy.red/wp-content/themes/sports-on-campus/assets/images/Volleyball.png" alt="volleyball "></li>
+                    </ul>
+                </div>
+                <?php the_content(); ?>
+            </div><!-- .entry-content -->
 
-	</main><!-- #main -->
+            <h3>programs & more</h3>
+            <div class="uni-program"> <?php echo CFS()->get('program'); ?>
+                <div class="read-more">
+                    <p>Read more</p>
+                </div>
+            </div>
+            <div class="uni-prerequisites">
+                <h3 class="secondary-heading">prerequisites for application</h3>
+                <div><?php echo CFS()->get('prerequisites'); ?></div>
+            </div>
+            <h3>facilities and quick links</h3>
+            <div class="uni-facilities"> <?php echo CFS()->get('facilities'); ?> </div>
+            <div class="uni-maps"> <?php echo CFS()->get('maps'); ?> </div>
+
+            <footer class="entry-footer">
+                <?php Sport_On_Campus_entry_footer(); ?>
+            </footer><!-- .entry-footer -->
+        </article><!-- #post-## -->
+        <?php endwhile; ?>
+
+    </main><!-- #main -->
 </div><!-- #primary -->
 
 
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
